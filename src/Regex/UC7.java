07b204955572371e90171c3f6431bdabc120defa
PRUDHVI-9 <prudhvirajathelli@gmail.com>
2023-05-10 22:55:06 +0530

    Added:To check  password  having atleast 1  numeric number .

---
+++ src/Regex/UC7.java
@@ -0,0 +1,64 @@
+package Regex;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UC7 {
+
+	/**
+	 * This method is created to check that password should contain least 1 numeric
+	 * number in it.
+	 * 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Scanner cs = new Scanner(System.in);
+		System.out.println("Enter Password:");
+		String str1 = cs.next();
+		isPassword(str1);
+
+		if (isPassword(str1)) {
+			System.out.println("Password " + str1 + " is valid.");
+		} else {
+			System.out.println("Password " + str1 + " is not valid.");
+
+		}
+		cs.close();
+	}
+
+	public static boolean isPassword(String string) {
+		/*
+		 * Regex to check valid Password .
+		 */
+		String regex = "[[A-Z]+[a-z]+[0-9]+]{8,}";
+
+		/*
+		 * To compile the Regex.
+		 */
+		Pattern p = Pattern.compile(regex);
+
+		/*
+		 * If the Password is empty,return false .
+		 */
+
+		if (string == null) {
+			return false;
+		}
+
+		/*
+		 * To find matching between given Password and regular expression.
+		 */
+
+		Matcher m = p.matcher(string);
+
+		/*
+		 * Return if the Password matched the ReGex.
+		 */
+
+		return m.matches();
+	}
+
+}
